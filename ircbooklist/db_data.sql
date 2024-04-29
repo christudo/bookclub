0335d221df65548f7dea99c0da8bcbe0c24ee86c
Christine DaCruz <cdacruz@pratt.edu>
2024-04-29 17:43:36 -0400

    description updates

--- ircbooklist/db_data.sql
+++ ircbooklist/db_data.sql
@@ -15,7 +15,7 @@ insert into authors (first_name, last_name) values ('Elizabeth', 'Weil');
 insert into authors (first_name, last_name) values ('Omar', 'Mohamed');
 
 
-insert into books values (default, 'The Refugees', 'In this collection of stories written over a 20-year period, Nguyen explores questions of identity, love, and family as experienced by immigrants.', 
+insert into books values (default, 'The Refugees', 'In this collection of stories written over a 20-year period, Nguyen explores questions of identity, love, and family as experienced by immigrants. Viet Thanh Nguyen shot to fame with his debut, Pulitzer-winning novel, The Sympathiser, and its sequel, The Committed. At the center of each story, Nguyen explores how it feels and what it means to be a refugee.', 
 2016, 2023, 951955956, '951955956_The Refugees_1.jpeg');
 insert into books values (default, 'Lupita Mañana', 'Lupita must leave Mexico for America after her  father''s  accidental death sinks her family into poverty. A keen reader all her life, and author of more than 50 books, Patricia Beatty is the embodiment of her passion for positive, impactful stories.', 
 1998, 2023, 982159393, '982159393_LupitaManana_2.jpeg');
@@ -27,17 +27,17 @@ insert into books values (default, 'The Ungrateful Refugee', 'This story begins 
 2020, 2023, 1139151247, '1139151247_TheUngratefulRefugee_5.jpeg');
 insert into books values (default, 'On Earth We''re Briefly Gorgeous', 'T.S. Eliot Award-winning poet Ocean Vuong describes the Vietnamese-American refugee experience in his semi-autobiographical first novel, a vivid, lyrical account of the legacy of conflict and trauma haunting three generations of a family.', 
 2021, 2023, 1253676116, '1253676116_OnEarthWereBrieflyGorgeous_6.jpeg');
-insert into books values (default, 'The Girl Who Smiled Beads: A Story of War and What Comes After', 'Clemantine Wamariya, working with Elizabeth Weil, tells the story of her childhood escape from the Rwandan massacre and her search for safety with her older sister.', 
+insert into books values (default, 'The Girl Who Smiled Beads: A Story of War and What Comes After', 'Clemantine Wamariya, working with Elizabeth Weil, tells the story of her childhood escape from the Rwandan massacre and her search for safety with her older sister. The siblings spend six years migrating across Africa until, as Clemantine turns 12, they gain refugee status and move to Chicago.', 
 2019, 2023, 1101930837, '1101930837_TheGirlWhoSmiledBeads_7.jpeg');
 insert into books values (default, 'The Kite Runner', 'This is a graphic novel that explores Mohamed and his brother''s life in Dadaab refugee camp, Kenya, told with beautiful illustrations that depict the reality for displaced children', 
 2011, 2023, 704895404, '704895404_TheKiteRunner_8.jpeg');
-insert into books values (default, 'The When Stars are Scattered', 'This is a graphic novel that explores Mohamed and his brother''s life in Dadaab refugee camp, Kenya, told with beautiful illustrations that depict the reality for displaced children.', 
+insert into books values (default, 'The When Stars are Scattered', 'This is a graphic novel that explores Mohamed and his brother''s life in Dadaab refugee camp, Kenya, told with beautiful illustrations that depict the reality for displaced children. Omar Mohamed and co-writer Victoria Jamieson powerfully depict what it''s like to be a refugee', 
 2020, 2023, 1109416697, '1109416697_WhenStarsareScattered_9.jpeg');
-insert into books values (default, 'We Are Displaced: My Journey and Stories from Refugee Girls Around the World', 'Part-memoir, part oral history, this book explores the multitudinous experiences of refugees, as well as Malala''s own story of displacement when a young child in Pakistan.', 
+insert into books values (default, 'We Are Displaced: My Journey and Stories from Refugee Girls Around the World', 'Part-memoir, part oral history, this book explores the multitudinous experiences of refugees, as well as Malala''s own story of displacement when a young child in Pakistan. Seen as an international symbol of the fight for girls'' education, Malala also works as a U.N. Messenger of Peace.', 
 2021, 2023, 1157050336, '1157050336_WeAreDisplaced_10.jpeg');
-insert into books values (default, 'Gravel Heart', 'As the book''s protagonist, Salim, moves from Zanzibar in the 1960s to London in the 1990s, he confronts issues of identity, belonging and betrayal.', 
+insert into books values (default, 'Gravel Heart', 'As the book''s protagonist, Salim, moves from Zanzibar in the 1960s to London in the 1990s, he confronts issues of identity, belonging and betrayal. Explains one of the book''s key moments, where Salim must decide to stay in London or return to Zanzibar', 
 2018, 2023, 1001540717, '1001540717_GravelHeart_11.jpeg');
-insert into books values (default, 'Girl at War', 'This powerful novel, set in former Yugoslavia, follows the adventures of a young girl whose only chance for survival is to escape war and rebuild a new life elsewhere.', 
+insert into books values (default, 'Girl at War', 'This powerful novel, set in former Yugoslavia, follows the adventures of a young girl whose only chance for survival is to escape war and rebuild a new life elsewhere. The tragic story conveys the complexities of civil strife and their effect on ordinary people living in what is now Croatia, Bosnia and Serbia.', 
 2016, 2023, 923572150, '923572150_GirlatWar_12.jpeg');
 insert into books values (default, 'Exit West', 'Magical realism conveys the strangeness of displacement in this short novel by Pakistani author Mohsin Hamid. A young couple escape their war-torn city through a mysterious portal, only to discover magical doors popping up around the world.', 
 2017, 2023, 951925351, '951925351_ExitWest_13.jpeg');
