9db06436833c62919f5c6468c835ef5d0ccf33c6
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2024-04-30 10:20:34 -0400

    Rik doing project folder cleanup
    I've also added a gitignore file and removed the node modules folder

--- ircbooklist/credentials.development.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "cookieSecret": process.env.COOKIESECRET || "MysuperSecretCookieSecret",
-    "postgres": {
-      "connectionString": process.env.DBCONNECTIONSTRING || 
-      "postgresql://postgres:P@ssw0rd@localhost:5432/ircbooklist"
-    }
-   }
